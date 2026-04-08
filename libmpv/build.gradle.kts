ae7c279203ba3b3be6c85a67e225d3b95a90659e
Tim <tymmesyde@gmail.com>
2026-04-08 18:55:18 +0200

    chore: v0.7.0

--- libmpv/build.gradle.kts
+++ libmpv/build.gradle.kts
@@ -54,7 +54,7 @@ mavenPublishing {
     coordinates(
         groupId = "com.github.Stremio",
         artifactId = "libmpv",
-        version = "0.6.2"
+        version = "0.7.0"
     )
 
     pom {
